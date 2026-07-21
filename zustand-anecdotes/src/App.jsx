560002669dd2645803e387c7bf88dacca6884920
Philemon Kasereka <kmboghaphilemon@gmail.com>
2026-07-21 11:35:29 +0300

    feat: complete exercise 6.2 anecdotes step1 with vote functionality

---
+++ zustand-anecdotes/src/App.jsx
@@ -0,0 +1,23 @@
+import { useAnecdotes, useAnecdoteActions } from "./store";
+
+const App = () => {
+  const anecdotes = useAnecdotes();
+  const { vote } = useAnecdoteActions();
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
